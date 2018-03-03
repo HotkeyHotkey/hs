e482ee3bdf5c933c997d1c87c60fb78389ee8769
HotkeyHotkey <31609972+HotkeyHotkey@users.noreply.github.com>
2018-03-03 09:23:04 +0500

    Update reg.php

--- functions/reg.php
+++ functions/reg.php
@@ -5,31 +5,32 @@
     $password = $_GET['password'];
     $r_password = $_GET['r-password'];
     $errors = array();
-    
-      if(trim($login) == '' || trim($email) == '' || trim($password) == '' || trim($r_password) == ''){
-        echo "noData";
-        array_push($errors, 'error');
-        return false;
-      }
-      if(hasOb('login', $login, 'users')){
-        echo "hasLogin";
-        array_push($errors, 'error');
-        return false;
-      }
-      if(!filter_var($email, FILTER_VALIDATE_EMAIL, FILTER_FLAG_PATH_REQUIRED)){
-        echo 'errorEmail';
-        array_push($errors, 'error');
-        return false;
-      }
-      if(hasOb('email', $email, 'users')){
-        echo 'hasEmail';
-        array_push($errors, 'error');
-        return false;
-      }
-      if($r_password != $password){
-        echo 'errorRPass';
-        array_push($errors, 'error');
-        return false;
+        if(true){
+          if(trim($login) == '' || trim($email) == '' || trim($password) == '' || trim($r_password) == ''){
+            echo "noData";
+            array_push($errors, 'error');
+            return false;
+          }
+          if(hasOb('login', $login, 'users')){
+            echo "hasLogin";
+            array_push($errors, 'error');
+            return false;
+          }
+          if(!filter_var($email, FILTER_VALIDATE_EMAIL, FILTER_FLAG_PATH_REQUIRED)){
+            echo 'errorEmail';
+            array_push($errors, 'error');
+            return false;
+          }
+          if(hasOb('email', $email, 'users')){
+            echo 'hasEmail';
+            array_push($errors, 'error');
+            return false;
+          }
+          if($r_password != $password){
+            echo 'errorRPass';
+            array_push($errors, 'error');
+            return false;
+          }
       }
       if(empty($errors)){
         echo "reg";
